7c654a67e3e63b01d28ea11e5bc5e25be829c932
Ramij Raj <rraj@jupiter.com>
2023-03-25 14:38:41 +0530

    upload button implemented in store details page

--- src/pages/Store/StoreDetails.jsx
+++ src/pages/Store/StoreDetails.jsx
@@ -5,6 +5,7 @@ import { ROUTES } from '../../routes/RouterConfig'
 
 import Footer from '../../components/Footer'
 import Header2 from '../../components/Header2'
+import UploadButton from '../../components/Button/UploadButton';
 
 
 
@@ -18,7 +19,7 @@ const StoreDetails = () => {
   }
 
 
- 
+
 
   return (
     <div className='Login'>
@@ -31,8 +32,8 @@ const StoreDetails = () => {
             <Container fluid="sm">
               <Row>
 
-              <Col sm='12'>
-              <Form.Group className="mb-3">
+                <Col sm='12'>
+                  <Form.Group className="mb-3">
                     <Form.Label className='font-semibold'>Store Name</Form.Label>
                     <Form.Control className='rounded-md h-10 mb-8' type="text" placeholder="Enter Store Name" />
                   </Form.Group>
@@ -40,23 +41,23 @@ const StoreDetails = () => {
                     <Form.Control className='rounded-md h-10 mb-8' type="text" placeholder="Enter Your Store Address" />
                   </Form.Group>
                 </Col>
-               
+
 
               </Row>
               <Row>
-              <Col sm='4'>
-              <Form.Group className="mb-3">
+                <Col sm='4'>
+                  <Form.Group className="mb-3">
                     <Form.Label className='font-semibold'>Latitude</Form.Label>
                     <Form.Control className='rounded-md h-10 mb-8' type="number" placeholder="Latitude" />
                   </Form.Group>
-              </Col>
-              <Col sm='4'>
-                
                 </Col>
-              <Col sm='4'>
-              
+                <Col sm='4'>
+
+                </Col>
+                <Col sm='4'>
+
                   <Form.Group className="mb-3">
-                  <Form.Label className='font-semibold'>Longitude</Form.Label>
+                    <Form.Label className='font-semibold'>Longitude</Form.Label>
                     <Form.Control className='rounded-md h-10 mb-8' type="number" placeholder="Longitude" />
                   </Form.Group>
                 </Col>
@@ -64,7 +65,7 @@ const StoreDetails = () => {
               </Row>
               <Row>
                 <Col sm='6'>
-                  
+
                   <Form.Group className="mb-3">
                     <Form.Label className='font-semibold'>Store Address (Locality)</Form.Label>
                     <Form.Control className='rounded-md h-10 mb-8' type="text" placeholder="Enter Street Number" />
@@ -80,24 +81,24 @@ const StoreDetails = () => {
                   <Form.Group className="mb-3">
                     <Form.Label className='font-semibold'>Add which category would you deliver</Form.Label>
                     <Form.Select aria-label="Select" className='rounded-md h-10 mb-8'>
-      <option>Select</option>
-      <option value="1">One</option>
-      <option value="2">Two</option>
-      <option value="3">Three</option>
-    </Form.Select>
+                      <option>Select</option>
+                      <option value="1">One</option>
+                      <option value="2">Two</option>
+                      <option value="3">Three</option>
+                    </Form.Select>
                   </Form.Group>
                   <Form.Group className="mb-3">
                     <Form.Label className='font-semibold'>Who Manage Store</Form.Label>
                     <Form.Select aria-label="Name" className='rounded-md h-10 mb-8'>
-      <option>Name</option>
-      <option value="1">One</option>
-      <option value="2">Two</option>
-      <option value="3">Three</option>
-    </Form.Select>
+                      <option>Name</option>
+                      <option value="1">One</option>
+                      <option value="2">Two</option>
+                      <option value="3">Three</option>
+                    </Form.Select>
                   </Form.Group>
                 </Col>
                 <Col sm='6'>
-                <Form.Group className="mb-3">
+                  <Form.Group className="mb-3">
                     <Form.Label className='font-semibold'>Store Address (City)</Form.Label>
                     <Form.Control className='rounded-md h-10 mb-8' type="text" placeholder="Enter City" />
                   </Form.Group>
@@ -107,29 +108,32 @@ const StoreDetails = () => {
                   </Form.Group>
                   <Form.Group className="mb-3">
                     <Form.Label className='font-semibold'>Upload Store Image</Form.Label>
-                    <Form.Control className='rounded-md h-10 mb-8' type="file" />
+                    {/* Custom button for uploading file  */}
+                    <UploadButton
+                      text='Upload Store Image'
+                    />
                   </Form.Group>
                   <Form.Group className="mb-3">
                     <Form.Label className='font-semibold'>Services</Form.Label>
                     <Form.Select aria-label="Select" className='rounded-md h-10 mb-8'>
-      <option>Select</option>
-      <option value="1">One</option>
-      <option value="2">Two</option>
-      <option value="3">Three</option>
-    </Form.Select>
+                      <option>Select</option>
+                      <option value="1">One</option>
+                      <option value="2">Two</option>
+                      <option value="3">Three</option>
+                    </Form.Select>
                   </Form.Group>
                 </Col>
-              
+
               </Row>
               <Row>
                 <Col sm={6}>
-                <Form.Group className="mb-3">
+                  <Form.Group className="mb-3">
                     <Form.Label className='font-semibold'>Opening Time</Form.Label>
                     <Form.Control className='rounded-md h-10 mb-8' type="time" placeholder="Enter City" />
                   </Form.Group>
                 </Col>
                 <Col sm={6}>
-                <Form.Group className="mb-3">
+                  <Form.Group className="mb-3">
                     <Form.Label className='font-semibold'>Closing Time</Form.Label>
                     <Form.Control className='rounded-md h-10 mb-8' type="time" placeholder="Enter City" />
                   </Form.Group>
@@ -137,50 +141,50 @@ const StoreDetails = () => {
               </Row>
               <Row>
                 <div>
-                <h2 className='font-semibold text-xl mb-2'>Mark Open Days</h2>
-                <h2 className='font-medium text-sm pb-4'>Don't forget to uncheck your off-day</h2>
+                  <h2 className='font-semibold text-xl mb-2'>Mark Open Days</h2>
+                  <h2 className='font-medium text-sm pb-4'>Don't forget to uncheck your off-day</h2>
                 </div>
-            
+
                 <Col sm={4}>
-                <Form.Group className="mb-3" controlId="c1">
-        <Form.Check type="checkbox" label="Monday" />
-      </Form.Group>
-      </Col>
-      <Col sm={4}>
-                <Form.Group className="mb-3" controlId="c2">
-        <Form.Check type="checkbox" label="Tuesday" />
-      </Form.Group>
-      </Col>
-      <Col sm={4}>
-                <Form.Group className="mb-3" controlId="c3">
-        <Form.Check type="checkbox" label="Wednesday" />
-      </Form.Group>
-      </Col>
-      <Col sm={4}>
-                <Form.Group className="mb-3" controlId="c4">
-        <Form.Check type="checkbox" label="Thursday" />
-      </Form.Group>
-      </Col>
-      <Col sm={4}>
-                <Form.Group className="mb-3" controlId="c5">
-        <Form.Check type="checkbox" label="Friday" />
-      </Form.Group>
-      </Col>
-      <Col sm={4}>
-                <Form.Group className="mb-3" controlId="c6">
-        <Form.Check type="checkbox" label="Saturday" />
-      </Form.Group>
-      </Col>
-      <Col sm={4}>
-                <Form.Group className="mb-3" controlId="c7">
-        <Form.Check type="checkbox" label="Sunday" />
-      </Form.Group>
-      </Col>
-      
+                  <Form.Group className="mb-3" controlId="c1">
+                    <Form.Check type="checkbox" label="Monday" />
+                  </Form.Group>
+                </Col>
+                <Col sm={4}>
+                  <Form.Group className="mb-3" controlId="c2">
+                    <Form.Check type="checkbox" label="Tuesday" />
+                  </Form.Group>
+                </Col>
+                <Col sm={4}>
+                  <Form.Group className="mb-3" controlId="c3">
+                    <Form.Check type="checkbox" label="Wednesday" />
+                  </Form.Group>
+                </Col>
+                <Col sm={4}>
+                  <Form.Group className="mb-3" controlId="c4">
+                    <Form.Check type="checkbox" label="Thursday" />
+                  </Form.Group>
+                </Col>
+                <Col sm={4}>
+                  <Form.Group className="mb-3" controlId="c5">
+                    <Form.Check type="checkbox" label="Friday" />
+                  </Form.Group>
+                </Col>
+                <Col sm={4}>
+                  <Form.Group className="mb-3" controlId="c6">
+                    <Form.Check type="checkbox" label="Saturday" />
+                  </Form.Group>
+                </Col>
+                <Col sm={4}>
+                  <Form.Group className="mb-3" controlId="c7">
+                    <Form.Check type="checkbox" label="Sunday" />
+                  </Form.Group>
+                </Col>
+
               </Row>
               <div className='text-center'>
-                                        <Button className='bg-[#0d6efd] rounded-full w-32 mt-12' variant="primary">Next</Button>
-                                    </div>
+                <Button className='bg-[#0d6efd] rounded-full w-32 mt-12' variant="primary">Next</Button>
+              </div>
             </Container>
           </Form>
 
